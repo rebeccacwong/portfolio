f6f246f29fdea9da96d69f9c1e3419de73357678
Rebecca Wong <rebeccacwong@berkeley.edu>
2019-12-30 10:09:31 -0500

    fixed css error on portfolio slides

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,7 @@
 var front = 0;	// front frame, index in const frames array
 var curr = 0;	// front frame, index in dynamic frames in html
 const frames = $(".frame td").toArray();
+const captions = $(".overlay td").toArray();
 console.log(frames);
 console.log(frames[0].innerHTML);
 
@@ -11,12 +12,13 @@ function slide() {
 	var html2 = "<td></td>";
 	var pos = $(".portfolio-slideshow").scrollLeft();
 	console.log(pos);
-	$(".portfolio-slideshow").animate({ scrollLeft: '+=150' }, 1000);
-	// $('.frame tr').find('td:eq(' + curr + '),th:eq(' + curr + ')').remove();
-	// $('.frame tr').find('td:eq(' + curr + '),th:eq(' + curr + ')').innerHTML = "";
-	console.log($(".frame tr"));
+	$(".portfolio-slideshow").animate({ scrollLeft: '+=400' }, 1000);
+	// $('table tr').find('td:eq(' + curr + '),th:eq(' + curr + ')').remove();
+	$('table tr').find('td:eq(' + curr + '),th:eq(' + curr + ')').innerHTML = "";
+	// console.log($(".frame tr"));
 	$('.frame tr')[0].innerHTML += html;
 	$('.frame tr')[1].innerHTML += html2;
+	$('.overlay tr')[0].innerHTML += "<td>" + captions[front].innerHTML + "</td>";
 	front = (front + 1) % Math.round(frames.length / 2);
 	console.log(front);
 	console.log($(".frame td"));
@@ -26,7 +28,7 @@ function slide() {
 
 // $(".portfolio-slideshow").scroll(function() {console.log("scrolled")})
 
-// setInterval(slide, 4000)
+setInterval(slide, 3000)
 
 /** NAVIGATION ANIMATION **/
 const navButton = document.querySelector('#navbar-button');
