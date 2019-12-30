3485c977912cd5e41cf4fd89e7999ac8c181d826
Rebecca Wong <rebeccacwong@berkeley.edu>
2019-12-30 15:48:39 -0500

    portfolio slideshow edits

--- portfolio.js
+++ portfolio.js
@@ -1,34 +1,73 @@
+/** INITIALIZATION **/
+$('html').css('display', 'none');
+$(document).ready(main);
+function main() {
+	$('html:not(.overlay-text)').fadeIn(800);
+	$('.overlay-text').css('display', 'none');
+	// $('.portfolio-slideshow').scrollLeft(400);
+}
+function toggleNav() {
+	$('.navbar').slideToggle();
+}
+
 /** PORTFOLIO CAROUSEL **/
 var front = 0;	// front frame, index in const frames array
 var curr = 0;	// front frame, index in dynamic frames in html
 const frames = $(".frame td").toArray();
 const captions = $(".overlay td").toArray();
-console.log(frames);
-console.log(frames[0].innerHTML);
+const imgs = $(".frame img");
 
+// for (i = 1; i < imgs.length; i++) {
+// 	$(imgs[i]).hover(function() {
+// 		console.log('hovered')
+// 		clearInterval(interval);
+// 	}, function() {
+// 		interval = setInterval(3000);
+// 		console.log('off')
+// 	})
+// }
 
 function slide() {
+	slideCarousel(400, 1);
+	// $(".portfolio-slideshow").scrollLeft(pos + 150);
+}
+
+function slideCarousel(amount, direction) {
+	clearInterval(interval);
 	var html = "<td>" + frames[front].innerHTML + "</td>";
 	var html2 = "<td></td>";
 	var pos = $(".portfolio-slideshow").scrollLeft();
 	console.log(pos);
-	$(".portfolio-slideshow").animate({ scrollLeft: '+=400' }, 1000);
+	if (direction == 1) {
+		$(".portfolio-slideshow").animate({ scrollLeft: '+=' + amount.toString() }, 1000);
+	} else {
+		$(".portfolio-slideshow").animate({ scrollLeft: '-=' + amount.toString() }, 1000);
+		console.log('reverse')
+	}
+	
 	// $('table tr').find('td:eq(' + curr + '),th:eq(' + curr + ')').remove();
-	$('table tr').find('td:eq(' + curr + '),th:eq(' + curr + ')').innerHTML = "";
+	// $('table tr').find('td:eq(' + curr + '),th:eq(' + curr + ')').innerHTML = "";
 	// console.log($(".frame tr"));
 	$('.frame tr')[0].innerHTML += html;
 	$('.frame tr')[1].innerHTML += html2;
 	$('.overlay tr')[0].innerHTML += "<td>" + captions[front].innerHTML + "</td>";
 	front = (front + 1) % Math.round(frames.length / 2);
+	// console.log($('frame img:last-child'));
+	// $('frame img:last-child').hover(function() {
+	// 	console.log('hovered')
+	// 	clearInterval(interval);
+	// }, function() {
+	// 	interval = setInterval(3000);
+	// })
 	console.log(front);
 	console.log($(".frame td"));
-	// $(".portfolio-slideshow").scrollLeft(pos + 150);
+	interval = setInterval(slide, 4000);
 }
 
 
 // $(".portfolio-slideshow").scroll(function() {console.log("scrolled")})
 
-setInterval(slide, 3000)
+var interval = setInterval(slide, 4000)
 
 /** NAVIGATION ANIMATION **/
 const navButton = document.querySelector('#navbar-button');
