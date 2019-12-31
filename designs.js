7ea39489521d478e4fe1e59830c141997580293d
Rebecca Wong <rebeccacwong@berkeley.edu>
2019-12-31 10:53:20 -0500

    added all design pages

--- designs.js
+++ designs.js
@@ -15,10 +15,29 @@ const docWidth = document.documentElement.clientWidth;
 
 nextBtn.addEventListener('click', changeSlide);
 
+/** HELPER FUNCTION **/
+function vhTOpx(value) {
+  var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+  var result = (y*value)/100;
+  return result;
+}
+
+
 function changeSlide() {
-	size = carouselImages[0].clientHeight;
+	var margin = carouselImages[0].style.marginTop.toString();
+	margin = margin.substring(0, margin.length - 2);
+	margin = vhTOpx(margin);
+	console.log('margin', margin, vhTOpx(margin));
+	var size = carouselImages[0].clientHeight + margin * 2;
 	// carouselContainer.css('height', (size * 100 / docWidth) + "vw");
-	console.log(carouselImages)
+	console.log(size);
+	// console.log(carouselImages)
 	if (counter % carouselImages.length == 0) {
 		counter = 1;
 		carouselSlide.css('transform', "none");
