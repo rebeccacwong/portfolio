e358b78c6eba215ca909ccae5434df8f42db37de
Rebecca Wong <rebeccacwong@berkeley.edu>
2024-06-27 10:18:35 -0700

    clear unused

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-/** NAVIGATION ANIMATION **/
-// function toggleNav() {
-//   $(".mynavbar").slideToggle();
-// }
-
-// const navButton = document.querySelector("#navbar-button");
-// navButton.addEventListener("click", toggleNav);
